a2b21b2100bdca39a1bbbb37d3cb1127058d62e3
daeun-shin <deshin@snack24h.com>
2021-07-28 12:51:41 +0900

    User 엔티티 생성 ( Role, BaseTimeEntity 함께 생성 )

---
+++ src/main/java/sde24/post/entity/User.java
@@ -0,0 +1,35 @@
+package sde24.post.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+public class User extends BaseTimeEntity{
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(Long id, String name, String email, Role role) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.role = role;
+    }
+}
