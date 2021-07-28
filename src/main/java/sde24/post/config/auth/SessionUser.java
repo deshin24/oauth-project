cc8e858338c3626589f569a3420c8b52960d878e
daeun-shin <deshin@snack24h.com>
2021-07-28 17:35:27 +0900

    SessionUser 생성

---
+++ src/main/java/sde24/post/config/auth/SessionUser.java
@@ -0,0 +1,20 @@
+package sde24.post.config.auth;
+
+import lombok.Getter;
+import sde24.post.entity.User;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    // 인증된 사용자 정보를 저장할 Dto
+    // 정상적으로 세션에 저장하고 꺼내오기 위해 Serializable class 를 implements 함
+
+    private String name;
+    private String email;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+    }
+}
