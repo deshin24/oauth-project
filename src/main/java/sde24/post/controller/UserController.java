9143abd57bdeb6890edce5e8b34a5966f7bb7bb5
daeun-shin <deshin@snack24h.com>
2021-07-29 10:18:18 +0900

    권한 변경 메소드 생성

--- src/main/java/sde24/post/controller/UserController.java
+++ src/main/java/sde24/post/controller/UserController.java
@@ -3,15 +3,13 @@ package sde24.post.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sde24.post.service.UserService;
+
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("user")
+@RequestMapping("/user")
 public class UserController {
 
     private final UserService userService;
@@ -19,8 +17,9 @@ public class UserController {
     /**
      * user 권한 변경
      */
-    @PutMapping("/{email}")
+    @PostMapping("/{email}")
     public ResponseEntity updateRole(@PathVariable String email){
+
         return new ResponseEntity(userService.updateRoleAdmin(email), HttpStatus.OK);
     }
 
--- src/main/java/sde24/post/service/UserService.java
+++ src/main/java/sde24/post/service/UserService.java
@@ -16,12 +16,13 @@ public class UserService {
     private final UserRepository userRepository;
 
     @Transactional
-    public Long updateRoleAdmin(String email){
+    public User updateRoleAdmin(String email){
 
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 회원 입니다."));
+
         user.updateRole(Role.ADMIN);
 
-        return user.getId();
+        return user;
     }
 }
